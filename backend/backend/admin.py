b1fca40c0e5ac34d8cd646826596acde5bd40176
Darren Robinson <darrenandamanda.robinson@gmail.com>
2026-05-26 09:38:16 +1000

    Celery

--- backend/backend/admin.py
+++ backend/backend/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django_celery_beat.models import PeriodicTasks
+
 from .models import *
 
 admin.site.register([User, ])
@@ -6,7 +8,7 @@ admin.site.register([BookingWeekly, WeeklyProgressSnapshot, Payment])
 admin.site.register([TutorAvailability, TutorProfile, TutorStudent, StudentProfile, DistributorProfile])
 admin.site.register([TeacherProfile, TeacherClass, TeacherClassStudent])
 admin.site.register([ClassAssessment, AssessmentStudentResult])
-admin.site.register([Skill, Note, Year])
+admin.site.register([Skill, Note, Year, PeriodicTasks])
 
 @admin.register(Template)
 class TemplateAdmin(admin.ModelAdmin):
